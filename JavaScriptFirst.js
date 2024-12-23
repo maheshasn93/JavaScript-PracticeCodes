527de1f9e52dc29d3aea051bfdd65e14da94680d
maheshasn93 <maheshasnreddy974@gmail.com>
2024-12-23 12:49:09 +0530

    Add datatypes code File

---
+++ JavaScriptFirst.js
@@ -0,0 +1,25 @@
+console.log("Hello World!..")
+
+a = 23;
+console.log(a, typeof a);
+
+a = 23.908;
+console.log(a, typeof a);
+
+a = true;
+console.log(a, typeof a);
+
+a = false;
+console.log(a, typeof a);
+
+a = 'K';
+console.log(a, typeof a);
+
+a = 'Kodnest';
+console.log(a, typeof a);
+
+function sum(num1, num2) {
+    return num1 + num2;
+}
+let result=sum(100, 200);
+console.log(result);
