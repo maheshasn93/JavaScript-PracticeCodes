23dc4c3c44c6612db7d0e513f089b4545c945123
maheshasn93 <maheshasnreddy974@gmail.com>
2024-12-24 09:44:07 +0530

    added diff way functions file

--- Diff_ways_functions.js
+++ Diff_ways_functions.js
@@ -3,3 +3,20 @@ function add(a, b){
     console.log(a+b);
 }
 add(100,200);
+
+// Function Expression
+const sub = function(a,b){
+    console.log(a-b);
+}
+sub(200, 100);
+//console.log(sub())
+
+//arrow function:
+const mul = (a, b) =>{
+    console.log(a * b);
+}
+mul(10, 20);
+
+const msg = () => "Hello World";
+let res = msg();
+console.log(res);
