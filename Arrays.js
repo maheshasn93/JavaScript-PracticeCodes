213cc0e012d09da865f92e1172a24ba494b55d72
maheshasn93 <maheshasnreddy974@gmail.com>
2024-12-26 10:12:18 +0530

    Update  Arrays file

--- Arrays.js
+++ Arrays.js
@@ -1,5 +1,9 @@
-var arr1 = [10, 20, 30, 40];
+var arr1 = [10, 20, 30, 40,20, 40, 60];
 console.log(arr1[0]);
 console.log(arr1[1]);
 console.log(arr1[2]);
 console.log(arr1[3]);
+
+for(i=0;i<arr1.length;i++){
+    console.log(`Element at index ${i} is`,arr1[i]);
+}
